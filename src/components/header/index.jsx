a65a3de7220225c404255d7c33e228bb856b2c2a
d3Yv3r <deyverson@astrotech.solutions>
2024-05-15 22:03:57 -0300

    Adicionado Chakra Ui

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,15 +1,9 @@
 import { CiUser } from "react-icons/ci";
 import { MdOutlinePets } from "react-icons/md";
 import logoName from "@/assets/images/logo_name.svg";
-import { useState } from "react";
+import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 
 export default function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleOpenMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <>
       <header className="flex justify-center bg-secondary-200 py-4 px-8">
@@ -22,21 +16,22 @@ export default function Header() {
               Add
             </button>
 
-            <button
-              className="text-white border-2 rounded-full p-2"
-              onClick={handleOpenMenu}
-            >
-              <CiUser size={24} />
-            </button>
+            <Menu>
+              <MenuButton>
+                <div className="text-white border-2 rounded-full p-2">
+                  <CiUser size={24} />
+                </div>
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Sign In</MenuItem>
+                <MenuItem onClick={() => console.log("Abriu Sign Up")}>
+                  Sign Up
+                </MenuItem>
+              </MenuList>
+            </Menu>
           </div>
         </div>
       </header>
-      {isOpen && (
-        <div className="flex flex-col gap-2 px-8 py-4 bg-secondary-100 rounded absolute z-50 right-2 top-20 translate-y-2">
-          <span>Login</span>
-          <span>Perfil</span>
-        </div>
-      )}
     </>
   );
 }
